5551218f1bfc2020fe4cb0432c26402aca0b18f9
brd6 <b.berdrigue@gmail.com>
2022-03-13 21:00:31 +0100

    fix: add missing base test

---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brd6\Test\NotionSdkPhp;
+
+use Ramsey\Dev\Tools\TestCase as BaseTestCase;
+
+/**
+ * A base test case for common test functionality
+ */
+class TestCase extends BaseTestCase
+{
+}
