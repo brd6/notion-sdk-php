739eeb1222e0fc19244c6f82804a3217e3389db9
brd6 <b.berdrigue@gmail.com>
2022-03-13 20:10:23 +0100

    test: base api test with request method

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -5,11 +5,166 @@ declare(strict_types=1);
 namespace Brd6\Test\NotionSdkPhp;
 
 use Brd6\NotionSdkPhp\Client;
+use Brd6\NotionSdkPhp\ClientOptions;
+use Brd6\NotionSdkPhp\Exception\ApiResponseException;
+use Brd6\NotionSdkPhp\RequestParameters;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+
+use function file_get_contents;
 
 class ClientTest extends TestCase
 {
     public function testInstance(): void
     {
         $this->assertInstanceOf(Client::class, new Client());
+    }
+
+    public function testRequestInvalidUrl(): void
+    {
+        $options = (new ClientOptions())
+            ->setAuth('secret_valid-auth');
+
+        $params = new RequestParameters();
+        $params
+            ->setMethod('GET')
+            ->setPath('invalid');
+
+        $httpClient = new MockHttpClient();
+        $httpClient->setResponseFactory([
+            new MockResponse(
+                (string) file_get_contents('tests/fixtures/client_request_invalid_url_400.json'),
+                [
+                    'http_code' => 400,
+                ],
+            ),
+        ]);
+
+        $options->setHttpClient($httpClient);
+
+        $client = new Client($options);
+
+        $this->expectException(ApiResponseException::class);
+        $this->expectExceptionMessage('Invalid request URL');
+
+        $client->request($params);
+    }
+
+    public function testRequestInvalidTokenApi(): void
+    {
+        $httpClient = new MockHttpClient();
+        $httpClient->setResponseFactory([
+            new MockResponse(
+                (string) file_get_contents('tests/fixtures/client_request_invalid_token_api_401.json'),
+                [
+                    'http_code' => 401,
+                ],
+            ),
+        ]);
+
+        $options = (new ClientOptions())
+            ->setAuth('secret_invalid-auth')
+            ->setHttpClient($httpClient);
+
+        $client = new Client($options);
+
+        $params = new RequestParameters();
+        $params
+            ->setMethod('GET')
+            ->setPath('pages/valid-page-id');
+
+        $this->expectException(ApiResponseException::class);
+        $this->expectExceptionMessage('API token is invalid');
+
+        $client->request($params);
+    }
+
+    public function testRequestMissingVersion(): void
+    {
+        $httpClient = new MockHttpClient();
+        $httpClient->setResponseFactory([
+            new MockResponse(
+                (string) file_get_contents('tests/fixtures/client_request_missing_version_400.json'),
+                [
+                    'http_code' => 400,
+                ],
+            ),
+        ]);
+
+        $options = (new ClientOptions())
+            ->setAuth('secret_valid-auth')
+            ->setHttpClient($httpClient);
+
+        $client = new Client($options);
+
+        $params = new RequestParameters();
+        $params
+            ->setMethod('GET')
+            ->setPath('pages/valid-page-id');
+
+        $this->expectException(ApiResponseException::class);
+        $this->expectExceptionMessage('Notion-Version header failed validation');
+
+        $client->request($params);
+    }
+
+    public function testRequestRetrievePage(): void
+    {
+        $httpClient = new MockHttpClient();
+        $httpClient->setResponseFactory([
+            new MockResponse(
+                (string) file_get_contents('tests/fixtures/client_request_retrieve_page_200.json'),
+                [
+                    'http_code' => 200,
+                ],
+            ),
+        ]);
+
+        $options = (new ClientOptions())
+            ->setAuth('secret_valid-auth')
+            ->setHttpClient($httpClient);
+
+        $client = new Client($options);
+
+        $params = new RequestParameters();
+        $params
+            ->setMethod('GET')
+            ->setPath('pages/b55c9c91-384d-452b-81db-d1ef79372b75');
+
+        $responseData = $client->request($params);
+
+        $this->assertArrayHasKey('id', $responseData);
+        $this->assertArrayHasKey('object', $responseData);
+        $this->assertEquals('b55c9c91-384d-452b-81db-d1ef79372b75', $responseData['id']);
+        $this->assertEquals('page', $responseData['object']);
+    }
+
+    public function testRequestRetrieveInvalidPage(): void
+    {
+        $httpClient = new MockHttpClient();
+        $httpClient->setResponseFactory([
+            new MockResponse(
+                (string) file_get_contents('tests/fixtures/client_request_retrieve_page_404.json'),
+                [
+                    'http_code' => 404,
+                ],
+            ),
+        ]);
+
+        $options = (new ClientOptions())
+            ->setAuth('secret_valid-auth')
+            ->setHttpClient($httpClient);
+
+        $client = new Client($options);
+
+        $params = new RequestParameters();
+        $params
+            ->setMethod('GET')
+            ->setPath('pages/4a808e6e-8845-4d49-a447-fb2a4c460f0f');
+
+        $this->expectException(ApiResponseException::class);
+        $this->expectExceptionMessage('Could not find page with ID: 4a808e6e-8845-4d49-a447-fb2a4c460f0f');
+
+        $client->request($params);
     }
 }
