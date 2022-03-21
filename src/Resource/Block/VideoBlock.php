bde839d6a0dbb031366f42027e26e68567acc79a
brd6 <b.berdrigue@gmail.com>
2022-03-21 20:27:00 +0100

    feat: add video block

---
+++ src/Resource/Block/VideoBlock.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brd6\NotionSdkPhp\Resource\Block;
+
+use Brd6\NotionSdkPhp\Exception\InvalidFileException;
+use Brd6\NotionSdkPhp\Exception\UnsupportedFileTypeException;
+use Brd6\NotionSdkPhp\Resource\AbstractBlock;
+use Brd6\NotionSdkPhp\Resource\AbstractFile;
+
+class VideoBlock extends AbstractBlock
+{
+    protected ?AbstractFile $video = null;
+
+    /**
+     * @throws UnsupportedFileTypeException
+     * @throws InvalidFileException
+     */
+    protected function initializeBlockProperty(): void
+    {
+        $data = (array) $this->getRawData()[$this->getType()];
+        $this->video = AbstractFile::fromRawData($data);
+    }
+
+    public function getVideo(): ?AbstractFile
+    {
+        return $this->video;
+    }
+
+    public function setVideo(?AbstractFile $video): self
+    {
+        $this->video = $video;
+
+        return $this;
+    }
+}
