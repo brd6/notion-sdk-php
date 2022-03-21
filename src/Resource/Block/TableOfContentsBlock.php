be476daaa5e2ab77d1567f2e55692a17d8e34595
brd6 <b.berdrigue@gmail.com>
2022-03-21 21:47:35 +0100

    feat: add table of contents block

---
+++ src/Resource/Block/TableOfContentsBlock.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brd6\NotionSdkPhp\Resource\Block;
+
+use Brd6\NotionSdkPhp\Exception\InvalidRichTextException;
+use Brd6\NotionSdkPhp\Exception\UnsupportedRichTextTypeException;
+use Brd6\NotionSdkPhp\Resource\AbstractBlock;
+use Brd6\NotionSdkPhp\Resource\Property\TableOfContentsProperty;
+
+class TableOfContentsBlock extends AbstractBlock
+{
+    protected ?TableOfContentsProperty $tableOfContents = null;
+
+    /**
+     * @throws InvalidRichTextException
+     * @throws UnsupportedRichTextTypeException
+     */
+    protected function initializeBlockProperty(): void
+    {
+        $data = (array) $this->getRawData()[$this->getType()];
+        $this->tableOfContents = TableOfContentsProperty::fromRawData($data);
+    }
+
+    public function getTableOfContents(): ?TableOfContentsProperty
+    {
+        return $this->tableOfContents;
+    }
+
+    public function setTableOfContents(?TableOfContentsProperty $tableOfContents): self
+    {
+        $this->tableOfContents = $tableOfContents;
+
+        return $this;
+    }
+}
---
+++ src/Resource/Property/TableOfContentsProperty.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brd6\NotionSdkPhp\Resource\Property;
+
+use Brd6\NotionSdkPhp\Resource\AbstractProperty;
+
+class TableOfContentsProperty extends AbstractProperty
+{
+    protected string $color = '';
+
+    public static function fromRawData(array $rawData): self
+    {
+        $property = new self();
+
+        $property->color = (string) $rawData['color'];
+
+        return $property;
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+}
