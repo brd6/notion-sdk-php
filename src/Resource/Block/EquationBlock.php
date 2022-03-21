36c1fb5fc939936c63dfb4a46e9f297b1cb066b3
brd6 <b.berdrigue@gmail.com>
2022-03-21 21:23:59 +0100

    feat: add equation block

---
+++ src/Resource/Block/EquationBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brd6\NotionSdkPhp\Resource\Block;
+
+use Brd6\NotionSdkPhp\Resource\AbstractBlock;
+use Brd6\NotionSdkPhp\Resource\Property\EquationProperty;
+
+class EquationBlock extends AbstractBlock
+{
+    protected ?EquationProperty $equation = null;
+
+    protected function initializeBlockProperty(): void
+    {
+        $data = (array) $this->getRawData()[$this->getType()];
+        $this->equation = EquationProperty::fromRawData($data);
+    }
+
+    public function getEquation(): ?EquationProperty
+    {
+        return $this->equation;
+    }
+
+    public function setEquation(?EquationProperty $equation): EquationBlock
+    {
+        $this->equation = $equation;
+
+        return $this;
+    }
+}
