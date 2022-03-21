ebbd44ed8dd8014a007f426a7042444e9d1e6e0f
brd6 <b.berdrigue@gmail.com>
2022-03-21 23:24:13 +0100

    feat: add template block

---
+++ src/Resource/Block/TemplateBlock.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brd6\NotionSdkPhp\Resource\Block;
+
+use Brd6\NotionSdkPhp\Exception\InvalidResourceException;
+use Brd6\NotionSdkPhp\Exception\InvalidResourceTypeException;
+use Brd6\NotionSdkPhp\Exception\InvalidRichTextException;
+use Brd6\NotionSdkPhp\Exception\UnsupportedRichTextTypeException;
+use Brd6\NotionSdkPhp\Resource\AbstractBlock;
+use Brd6\NotionSdkPhp\Resource\Property\TemplateProperty;
+
+class TemplateBlock extends AbstractBlock
+{
+    protected ?TemplateProperty $template = null;
+
+    /**
+     * @throws InvalidResourceException
+     * @throws InvalidResourceTypeException
+     * @throws InvalidRichTextException
+     * @throws UnsupportedRichTextTypeException
+     */
+    protected function initializeBlockProperty(): void
+    {
+        $data = (array) $this->getRawData()[$this->getType()];
+
+        /** @var TemplateProperty $property */
+        $property = TemplateProperty::fromRawData($data);
+
+        $this->template = $property;
+    }
+
+    public function getTemplate(): ?TemplateProperty
+    {
+        return $this->template;
+    }
+
+    public function setTemplate(?TemplateProperty $template): self
+    {
+        $this->template = $template;
+
+        return $this;
+    }
+}
---
+++ src/Resource/Property/TemplateProperty.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brd6\NotionSdkPhp\Resource\Property;
+
+use Brd6\NotionSdkPhp\Resource\AbstractParagraphProperty;
+
+class TemplateProperty extends AbstractParagraphProperty
+{
+}
