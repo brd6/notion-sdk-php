5e5e70ccc6e5160f4746440a07da1edceaef7b8c
brd6 <b.berdrigue@gmail.com>
2022-03-21 20:21:43 +0100

    feat: add embed block

---
+++ src/Resource/Block/EmbedBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brd6\NotionSdkPhp\Resource\Block;
+
+use Brd6\NotionSdkPhp\Resource\AbstractBlock;
+use Brd6\NotionSdkPhp\Resource\Property\EmbedProperty;
+
+class EmbedBlock extends AbstractBlock
+{
+    protected ?EmbedProperty $embed = null;
+
+    protected function initializeBlockProperty(): void
+    {
+        $data = (array) $this->getRawData()[$this->getType()];
+        $this->embed = EmbedProperty::fromRawData($data);
+    }
+
+    public function getEmbed(): ?EmbedProperty
+    {
+        return $this->embed;
+    }
+
+    public function setEmbed(?EmbedProperty $embed): self
+    {
+        $this->embed = $embed;
+
+        return $this;
+    }
+}
