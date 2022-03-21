12bb55b61ed59442f75e62654e87a94190529fe3
brd6 <b.berdrigue@gmail.com>
2022-03-21 23:34:38 +0100

    feat: add link to page block

---
+++ src/Resource/Property/LinkToPageProperty.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brd6\NotionSdkPhp\Resource\Property;
+
+use Brd6\NotionSdkPhp\Resource\AbstractProperty;
+
+class LinkToPageProperty extends AbstractProperty
+{
+    protected string $type = '';
+
+    protected ?string $pageId = null;
+    protected ?string $databaseId = null;
+
+    public static function fromRawData(array $rawData): self
+    {
+        $property = new self();
+
+        $property->type = (string) $rawData['type'];
+        $property->pageId = isset($rawData['page_id']) ? (string) $rawData['page_id'] : null;
+        $property->databaseId = isset($rawData['database_id']) ? (string) $rawData['database_id'] : null;
+
+        return $property;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getPageId(): ?string
+    {
+        return $this->pageId;
+    }
+
+    public function setPageId(?string $pageId): self
+    {
+        $this->pageId = $pageId;
+
+        return $this;
+    }
+
+    public function getDatabaseId(): ?string
+    {
+        return $this->databaseId;
+    }
+
+    public function setDatabaseId(?string $databaseId): self
+    {
+        $this->databaseId = $databaseId;
+
+        return $this;
+    }
+}
