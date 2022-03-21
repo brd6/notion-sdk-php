543ea3dcd1333bfbc7bc7cb4ba8dcf2b0637b27f
brd6 <b.berdrigue@gmail.com>
2022-03-21 20:32:31 +0100

    feat: add pdf block

---
+++ src/Resource/Block/PdfBlock.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brd6\NotionSdkPhp\Resource\Block;
+
+use Brd6\NotionSdkPhp\Exception\InvalidFileException;
+use Brd6\NotionSdkPhp\Exception\UnsupportedFileTypeException;
+use Brd6\NotionSdkPhp\Resource\AbstractBlock;
+use Brd6\NotionSdkPhp\Resource\AbstractFile;
+
+class PdfBlock extends AbstractBlock
+{
+    protected ?AbstractFile $pdf = null;
+
+    /**
+     * @throws UnsupportedFileTypeException
+     * @throws InvalidFileException
+     */
+    protected function initializeBlockProperty(): void
+    {
+        $data = (array) $this->getRawData()[$this->getType()];
+        $this->pdf = AbstractFile::fromRawData($data);
+    }
+
+    public function getPdf(): ?AbstractFile
+    {
+        return $this->pdf;
+    }
+
+    public function setPdf(?AbstractFile $pdf): self
+    {
+        $this->pdf = $pdf;
+
+        return $this;
+    }
+}
