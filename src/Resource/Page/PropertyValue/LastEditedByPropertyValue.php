f856d3e0a8086968766c6a389c27f82d6787e9a2
brd6 <b.berdrigue@gmail.com>
2022-03-27 16:07:56 +0200

    feat: add last edited by property value

---
+++ src/Resource/Page/PropertyValue/LastEditedByPropertyValue.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brd6\NotionSdkPhp\Resource\Page\PropertyValue;
+
+use Brd6\NotionSdkPhp\Resource\Page\AbstractPropertyValue;
+use Brd6\NotionSdkPhp\Resource\User\AbstractUser;
+
+class LastEditedByPropertyValue extends AbstractPropertyValue
+{
+    protected ?AbstractUser $lastEditedBy = null;
+
+    protected function initialize(): void
+    {
+        $this->lastEditedBy = AbstractUser::fromRawData((array) $this->getRawData()[$this->getType()]);
+    }
+
+    public function getLastEditedBy(): ?AbstractUser
+    {
+        return $this->lastEditedBy;
+    }
+
+    public function setLastEditedBy(?AbstractUser $lastEditedBy): self
+    {
+        $this->lastEditedBy = $lastEditedBy;
+
+        return $this;
+    }
+}
