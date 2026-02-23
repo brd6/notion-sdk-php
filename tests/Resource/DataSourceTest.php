f093b27c19d1b4fcc7919c57153f0771967d9189
brd6 <b.berdrigue@gmail.com>
2026-02-23 20:01:06 +0100

    fix(test): avoid PHP 8 nullsafe operator
    Use explicit assertions instead of ?-> so the test suite parses on PHP 7.4.

--- tests/Resource/DataSourceTest.php
+++ tests/Resource/DataSourceTest.php
@@ -36,10 +36,10 @@ class DataSourceTest extends TestCase
 
         $this->assertNotEmpty($dataSource->getId());
         $this->assertEquals(DataSource::RESOURCE_TYPE, $dataSource->getObject());
-        $this->assertEquals('database_id', $dataSource->getParent()?->getType());
-        $this->assertEquals('page_id', $dataSource->getDatabaseParent()?->getType());
         $this->assertInstanceOf(DatabaseIdParent::class, $dataSource->getParent());
         $this->assertInstanceOf(PageIdParent::class, $dataSource->getDatabaseParent());
+        $this->assertEquals('database_id', $dataSource->getParent()->getType());
+        $this->assertEquals('page_id', $dataSource->getDatabaseParent()->getType());
         $this->assertFalse($dataSource->isInTrash());
         $this->assertFalse($dataSource->isArchived());
         $this->assertGreaterThan(0, count($dataSource->getProperties()));
@@ -48,7 +48,7 @@ class DataSourceTest extends TestCase
         $projectsProperty = $dataSource->getProperties()['Projects'];
         $this->assertSame(
             'a42a62ed-9b51-4b98-9dea-ea6d091bc508',
-            $projectsProperty->getRelation()?->getDataSourceId(),
+            $projectsProperty->getRelation()->getDataSourceId(),
         );
         $this->assertNotEmpty($dataSource->toArray());
     }
