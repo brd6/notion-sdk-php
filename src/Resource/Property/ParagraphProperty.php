d242cb386cf3335c0af4471e1c2abf955d890ccd
brd6 <b.berdrigue@gmail.com>
2026-09-07 17:09:06 +0200

    fix(blocks): preserve optional paragraph icons

--- src/Resource/Property/ParagraphProperty.php
+++ src/Resource/Property/ParagraphProperty.php
@@ -4,6 +4,30 @@ declare(strict_types=1);
 
 namespace Brd6\NotionSdkPhp\Resource\Property;
 
+use Brd6\NotionSdkPhp\Resource\File\AbstractFile;
+
 class ParagraphProperty extends AbstractParagraphProperty
 {
+    protected ?AbstractFile $icon = null;
+
+    public static function fromRawData(array $rawData): self
+    {
+        /** @var self $property */
+        $property = parent::fromRawData($rawData);
+        $property->icon = isset($rawData['icon']) ? AbstractFile::fromRawData((array) $rawData['icon']) : null;
+
+        return $property;
+    }
+
+    public function getIcon(): ?AbstractFile
+    {
+        return $this->icon;
+    }
+
+    public function setIcon(?AbstractFile $icon): self
+    {
+        $this->icon = $icon;
+
+        return $this;
+    }
 }
